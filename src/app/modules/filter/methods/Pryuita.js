2d5000a90256e657fd263b42e6fbcb2994424ff5
Eugene Nemarsky <niamarsky@gmail.com>
2016-10-01 18:34:40 +0300

    Pryuita

--- src/app/modules/filter/methods/Pryuita.js
+++ src/app/modules/filter/methods/Pryuita.js
@@ -8,9 +8,25 @@ export default class Pryuita extends FilterInterface{
 
     filter(){
         var {imageData} = this;
-        var pixeles = imageData.data;
-        console.log(pixeles);
+        var pixels = imageData.data;
+        for (var i = 0, il = pixels.length; i < il; i += 4) {
+            var color = Math.random() * 255;
+            pixels[i] = pixels[i] * color / 255;
+            pixels[i + 1] = pixels[i + 1] * color / 255;
+            pixels[i + 2] = pixels[i + 2] * color / 255;
+        }
         return imageData;
     }
 
+    gx = [
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1],
+    ];
+
+    gy = [
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1],
+    ];
 }
