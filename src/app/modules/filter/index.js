bb67d37f76b9ec966d11298597158399bd6a58f5
Eugene Nemarsky <niamarsky@gmail.com>
2016-10-01 15:20:32 +0300

    Add filter interface

--- src/app/modules/filter/index.js
+++
@@ -1,7 +0,0 @@
-export default class Filter{
-
-    constructor(image){
-        this.image = image;
-    }
-
-}
---
+++ src/app/modules/filter/methods/Pryuita.js
@@ -0,0 +1,13 @@
+import FilterInterface from '../interface/FilterInterface';
+
+export default class Pryuita extends FilterInterface{
+
+    constructor(imageCtxDate){
+        super(imageCtxDate);
+    }
+
+    filter(){
+
+    }
+
+}
