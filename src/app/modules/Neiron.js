f226d72f76882a6a51f7aca93e56dbf8369fde0e
Eugene Nemarsky <niamarsky@gmail.com>
2016-12-19 01:27:01 +0300

    Добавлена НС с весовыми коэфицентами. Сделано обучение по Яблокам и апельсинам

--- src/app/modules/Neiron.js
+++ src/app/modules/Neiron.js
@@ -3,6 +3,10 @@ import _foreach from 'lodash/forEach';
 export default class Neiron {
 
     fruit = {};
+    coeficent = {
+        factor: 1,
+        color: 2,
+    };
 
     teach(fruit, rule){
         if(this.fruit[fruit] == undefined){
@@ -19,24 +23,50 @@ export default class Neiron {
     }
 
     search(rule){
-        var candidate = {
-            factor: 1000,
-            color: 1000,
-            name: '',
+        var candidates = {
+
         };
         _foreach(this.fruit, (fruit, key)=>{
+            candidates[key] = [];
             _foreach(fruit, (value)=>{
-                let _color = Math.abs(value.color.red - rule.color.red)
+                candidates[key].push({
+                    factor: Math.abs(value.factor - rule.factor),
+                    color:  Math.abs(value.color.red - rule.color.red)
                     + Math.abs(value.color.green - rule.color.green)
-                    + Math.abs(value.color.blue - rule.color.blue);
-                if(candidate.factor > Math.abs(value.factor - rule.factor)
-                    && candidate.color > _color){
-                    candidate.factor = Math.abs(value.factor - rule.factor);
-                    candidate.color = _color;
-                    console.log(key, (Math.abs(value.factor - rule.factor)), _color);
-                    candidate.name = key;
-                }
+                    + Math.abs(value.color.blue - rule.color.blue)
+                });
             })
+        });
+        var candidatesNew = {};
+        _foreach(candidates,(fruit, key)=>{
+            var result = {
+                factor: 0,
+                color: 0,
+            };
+            _foreach(fruit,(value)=>{
+                let temp = {
+                    factor: 100 - (value.factor * 100),
+                    color: 100 - (value.color * 100 / 765),
+                };
+                if(result.factor < temp.factor){
+                    result.factor = temp.factor;
+                }
+                if(result.color < temp.color){
+                    result.color = temp.color;
+                }
+            });
+            candidatesNew[key] = result;
+        });
+        var candidate = {
+            value: 0,
+            name: 'HZ',
+        };
+        console.log(candidatesNew);
+        _foreach(candidatesNew, (value,key)=>{
+            if(candidate.value < ((value.factor * this.coeficent.factor) + (value.color * this.coeficent.color))){
+                candidate.value = ((value.factor * this.coeficent.factor) + (value.color * this.coeficent.color));
+                candidate.name = key;
+            }
         });
         console.log(candidate);
         return candidate.name;
